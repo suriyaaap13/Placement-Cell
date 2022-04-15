e5c1f9b52c97288dbbfcabef63f276fe418294d1
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-15 17:19:41 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const express = require('express');
-const dotenv = require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
@@ -8,4 +8,4 @@ const PORT = 3000;
 app.listen(PORT, function(err){
     if(err){console.log("Error in listening to the Server", err);}
     console.log(`Server is up and running in Port ${PORT}`);
-})
+});
