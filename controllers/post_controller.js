0f14e389f9b1937f1665c70ca2d1be954a3f18a9
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-17 23:47:40 +0530

    create student form and pass the value of the form to the server

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,6 +1,10 @@
 module.exports.studentData = (req, res)=>{
-    console.log(req.body);
     return res.render('add_student', {
         title: "Add Student Form"
     });
+}
+module.exports.create = (req, res)=>{
+    console.log(req.body);
+    console.log(req.body.doi);
+    return;
 }
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,6 @@ const postController = require('../controllers/post_controller');
 
 // posts student data to the db
 router.get('/student', passport.checkAuthentication, postController.studentData);
+router.post('/create', postController.create);
 
 module.exports = router;
