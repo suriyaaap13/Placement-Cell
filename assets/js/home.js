6662fced10aecbe02b473d15c951896f1fc39af2
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-25 22:15:29 +0530

    Bring the student form to the home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,5 +2,4 @@
     $('#add-student').addClass("btn btn-primary me-2");
     $('#create-interview').addClass("btn btn-outline-primary me-2");
     $('#sign-out').addClass("btn btn-danger");
-    
 }
