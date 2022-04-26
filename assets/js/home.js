7758408e4f6d6c6866b56775f347885aa88f471b
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-26 10:56:15 +0530

    Complete designing the data table of students

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,10 @@
 {
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
+
     $('#add-student').addClass("btn btn-primary me-2");
     $('#create-interview').addClass("btn btn-outline-primary me-2");
     $('#sign-out').addClass("btn btn-danger");
+
 }
