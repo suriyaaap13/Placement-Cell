f5879c18e16d33913bce872607fceed74deb626b
suriyaaap13 <suriyaaap13@gmail.com>
2022-05-01 11:21:12 +0530

    xxyzz

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.set("layout extractScripts", true);
 app.use(session({
     name: 'PlacementCell',
     // Change before deployment
-    secret: pennygetyourownwifinospaces,
+    secret: "pennygetyourownwifinospaces",
     saveUninitialized: false,
     resave: false,
     cookie: {
